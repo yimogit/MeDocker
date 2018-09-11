2b2401eaf711c223da7b53f0ac5f70aa24f6a124
易墨 <1280160831@qq.com>
2018-09-11 11:29:09 +0800

    Update and rename dockker-clear.sh to docker-clear.sh

---
+++ docker-clear.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo '删除孤立的容器，docker>1.3'
+docker container prune
--- dockker-clear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'stop container'
-docker stop $(docker ps -a -q) 
-
-echo 'rmi container'
-docker rm $(docker ps -a -q)
-
-echo 'delete none images'
-docker rmi --force $(docker images | grep none | awk '{print $3}')
